fbc47a5adc1186ac0641ef913e532490360ed494
Denis Tarulli <132310271+DenisTarulli@users.noreply.github.com>
2024-07-27 22:56:25 -0300

    Player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,12 +3,29 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float extraRayHeight;
     private Vector2 moveDirection;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private BoxCollider2D boxCollider;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+    }
 
     private void Update()
     {
         Movement();
         FlipSprite();
+        SetAnimations();
+
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && IsGrounded())
+            Jump();
     }
 
     private void Movement()
@@ -26,5 +43,35 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = Vector3.one;
         else if (moveDirection.x < 0f)
             transform.localScale = new(-1f, 1f, 1f);
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
+
+        animator.SetTrigger("Jump");
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, extraRayHeight, groundLayer);
+
+        return raycastHit.collider != null;
+    }
+
+    private void SetAnimations()
+    {
+        float xInput = moveDirection.x;
+        float yInput = rb.velocity.y;
+
+        if (xInput != 0f)
+            animator.SetBool("Moving", true);
+        else
+            animator.SetBool("Moving", false);
+
+        if (yInput == 0f)
+            animator.SetBool("Grounded", true);
+        else
+            animator.SetBool("Grounded", false);
     }
 }
