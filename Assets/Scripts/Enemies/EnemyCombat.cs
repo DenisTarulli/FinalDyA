6399a35648cb2d711a83aa489c073a5f47b07d2c
Denis Tarulli <132310271+DenisTarulli@users.noreply.github.com>
2024-07-28 22:18:22 -0300

    Enemy & items basic

---
+++ Assets/Scripts/Enemies/EnemyCombat.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyCombat : MonoBehaviour
+{
+    [SerializeField] private int maxHealth;
+    [SerializeField] private int damage;
+    [SerializeField] private float attackRange;
+    [SerializeField] private float attackHitDelay;
+    [SerializeField] private float attackTriggerDistance;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private float attackSpeed;
+    [SerializeField] private float extraDelayUntilSpriteFlip;
+    private Animator animator;
+    private float nextTimeToAttack;
+    private float distanceToPlayer;
+    private int currentHealth;
+    private bool isAttacking;
+    private Transform playerPosition;
+
+    private void Start()
+    {
+        playerPosition = FindObjectOfType<PlayerCombat>().transform;
+        animator = GetComponentInChildren<Animator>();
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        if (playerPosition == null) return;
+
+        distanceToPlayer = Vector2.Distance(transform.position, playerPosition.position);
+        
+        if (distanceToPlayer <= attackTriggerDistance && !isAttacking && Time.time >= nextTimeToAttack)
+        {
+            TriggerAttack();
+        }
+    }
+
+    public void TakeDamage(int damageToTake)
+    {
+        currentHealth -= damageToTake;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+        else if (!isAttacking)
+            animator.SetTrigger("Hurt");
+    }
+
+    private void TriggerAttack()
+    {
+        isAttacking = true;
+        nextTimeToAttack = Time.time + 1f / attackSpeed;
+
+        animator.SetTrigger("Attack");
+
+        StartCoroutine(AttackAction());
+    }
+
+    private IEnumerator AttackAction()
+    {
+        yield return new WaitForSeconds(attackHitDelay);
+
+        Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, playerLayer);
+
+        foreach (Collider2D player in hitPlayer)
+        {
+            player.GetComponent<PlayerCombat>().TakeDamage(damage);
+        }
+
+        yield return new WaitForSeconds(extraDelayUntilSpriteFlip);
+
+        isAttacking = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    private void Die()
+    {
+        animator.SetTrigger("Die");
+
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+}
--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -6,13 +6,20 @@ public class PlayerCombat : MonoBehaviour
     [SerializeField] private float attackSpeed;
     [SerializeField] private float attackHitDelay;
     [SerializeField] private float extraDelayUntilSpriteFlip;
+    [SerializeField] private float attackRange;
+    [SerializeField] private int maxHealth;
+    [SerializeField] private int damage;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private LayerMask enemyLayer;
     private Animator animator;
+    private float currentHealth;
     private float nextTimeToAttack;
 
     public static bool isAttacking;
 
     private void Start()
     {
+        currentHealth = maxHealth;
         animator = GetComponentInChildren<Animator>();
     }
 
@@ -36,10 +43,30 @@ public class PlayerCombat : MonoBehaviour
 
         yield return new WaitForSeconds(attackHitDelay);
 
-        // Check hits
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<EnemyCombat>().TakeDamage(damage);
+        }
 
         yield return new WaitForSeconds(extraDelayUntilSpriteFlip);
 
         isAttacking = false;
+    }
+
+    public void TakeDamage(int damageToTake)
+    {
+        currentHealth -= damageToTake;
+
+        if (currentHealth <= 0)
+            Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) return; 
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
